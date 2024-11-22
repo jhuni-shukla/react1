045160551981013282742e661e4867561f52e144
jhuni-shukla <jhuni3040@gmail.com>
2024-11-22 20:50:43 +0530

    Initial commit

---
+++ react1/src/About.js
@@ -0,0 +1,12 @@
+function Abc()
+{    
+    return " I am default export";
+}
+
+export default Abc;
+
+
+export function Abc2()
+{    
+    return <h1>"I am name export and not export me here like above"</h1>;
+}
